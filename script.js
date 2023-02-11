97cb62d69e87bf0a4e0ac171a6f50d8987a80db3
prudhvi1601 <prudhvi1601@gmail.com>
2023-02-11 23:33:51 +0530

    array of student objects

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ let arr = [
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
-function PrintStudentswithMap() {
-  //Write your code here , just console.log
-}
+let newArr = arr.map(function PrintStudentswithMap(student) {
+    return student.marks>50;
+});
+console.log(newArr);
 
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-}
+let newValue = arr.forEach(function PrintStudentsbyForEach(score){
+  return score.marks;
+});
+console.log(newValue);
 
 function addData() {
   //Write your code here, just console.log
